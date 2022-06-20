720395af8f0b1033d026e22e785966f64c46b5ba
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-06-20 18:10:16 +0300

    Purpose and use of html elements

---
+++ Bootstrap_Learning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bootstrap_Learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
